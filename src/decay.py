60851b4e66b1ea6319e63a448e17f5668d1928aa
Leonard Kleinhans <leonard.kleinhans42@gmail.com>
2016-11-04 13:45:59 +0100

    Some comments for decay module

--- src/decay.py
+++ src/decay.py
@@ -1,6 +1,10 @@
 import abc
 
+
 class DecayFunction(metaclass=abc.ABCMeta):
+    """
+    Base class for decay functions, implemented with a stregy design pattern
+    """
     def __init__(self, init_value):
         self._value = init_value
 
@@ -12,11 +16,19 @@ class DecayFunction(metaclass=abc.ABCMeta):
     def decay(self):
         pass
 
+
 class StaticDecay(DecayFunction):
+    """
+    Not really a decay function. But since asked for, implemented as one.
+    """
     def decay(self):
         pass
 
+
 class LinearDecay(DecayFunction):
+    """
+    Decaying with a constant rate
+    """
     def __init__(self, start_value, rate):
         self.rate = rate
         super().__init__(start_value)
@@ -26,6 +38,9 @@ class LinearDecay(DecayFunction):
 
 
 class ExponentialDecay(DecayFunction):
+    """
+    Decaying exponential.
+    """
     def __init__(self, start_value, rate):
         self.rate = rate
         super().__init__(start_value)
