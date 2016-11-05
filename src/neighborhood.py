0a4507cd906db3a47b52f623be9b0c964086d941
Leonard Kleinhans <leonard.kleinhans42@gmail.com>
2016-11-05 21:04:32 +0100

    Add bubble neighborhood function

--- src/neighborhood.py
+++ src/neighborhood.py
@@ -1,7 +1,14 @@
 import math
 
+
 def gaussian(d, sigma):
     p = math.pow(sigma,2)
     if p == 0:
         return 0
     return math.exp(-d/(2*p))
+
+
+def bubble(d, sigma):
+    if d <= sigma:
+        return 1
+    return 0
